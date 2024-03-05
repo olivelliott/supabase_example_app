40e4353aeb257d7d941055917810a330535d6bd9
Olivia Elliott <olivelliott48@gmail.com>
2024-03-04 19:53:47 -0500

    build callback and signout routes

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,19 +1,25 @@
-import { createClient } from "@/utils/supabase/server";
-import { NextResponse } from "next/server";
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: Request) {
-  // The `/auth/callback` route is required for the server-side auth flow implemented
-  // by the SSR package. It exchanges an auth code for the user's session.
-  // https://supabase.com/docs/guides/auth/server-side/nextjs
-  const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get("code");
-  const origin = requestUrl.origin;
+/**  
+
+In the following code snippet, we perform the following steps:
+
+Retrieve the code sent back from the Supabase Auth server using the code query parameter.
+Exchange this code for a session, which we store in our chosen storage mechanism (in this case, cookies).
+Finally, we redirect the user to the account page.
+
+*/
+export async function GET(req: NextRequest) {
+  const cookieStore = cookies()
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+  const { searchParams } = new URL(req.url)
+  const code = searchParams.get('code')
 
   if (code) {
-    const supabase = createClient();
-    await supabase.auth.exchangeCodeForSession(code);
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
-  // URL to redirect to after sign up process completes
-  return NextResponse.redirect(`${origin}/protected`);
+  return NextResponse.redirect(new URL('/account', req.url))
 }
